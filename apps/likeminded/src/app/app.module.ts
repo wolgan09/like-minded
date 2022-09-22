11adde4546cf44ad76d3a22d06b20bb11e0b41e1
Vivek Kumar Singh <vivek.inboxme@gmail.com>
2022-09-22 18:57:08 +0530

    Added angular material package

--- apps/likeminded/src/app/app.module.ts
+++ apps/likeminded/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule ,MatToolbarModule,
+     NoopAnimationsModule,MatIconModule,
+     MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
